2ee6e2eaa59f3f9b7a54a914772bf2569ca0e410
VanGoghAlex <efy.reg@gmail.com>
2025-01-12 00:17:18 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,12 +9,12 @@ from oauth2client.service_account import ServiceAccountCredentials
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Ідентифікатор таблиці Google Sheets (взяти з URL таблиці)
+# Ідентифікатор таблиці Google Sheets
 SHEET_ID = "1G1TvM6BMakQegginA4W8ZVsqTqaflbTMHwWCFG0nvdI"
 
 # Ініціалізація Google Sheets API
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("/etc/secrets/service_account.json", scope)
 client = gspread.authorize(creds)
 
 # Ініціалізація Telegram Token
@@ -22,7 +22,7 @@ TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 # Функція для запису ID менеджера в Google Таблицю
 def add_manager_id_to_sheet(user_name, user_id):
-    sheet = client.open_by_key(SHEET_ID).sheet1  # Відкрити таблицю (перший аркуш)
+    sheet = client.open_by_key(SHEET_ID).sheet1  # Відкрити таблицю
     data = sheet.get_all_values()  # Отримати всі дані таблиці
     existing_ids = [row[0] for row in data]  # Зібрати список існуючих ID
     
@@ -33,7 +33,7 @@ def add_manager_id_to_sheet(user_name, user_id):
 def start(update: Update, context: CallbackContext):
     user_name = update.effective_user.first_name
     user_id = update.effective_user.id
-    update.message.reply_text(f"Хай, {user_name}! Твій Telegram ID: {user_id}")
+    update.message.reply_text(f"Привіт, {user_name}! Твій Telegram ID: {user_id}")
     
     # Спроба записати ID в таблицю
     try:
@@ -51,9 +51,6 @@ if __name__ == "__main__":
     # Додаємо обробник команди /start
     dp.add_handler(CommandHandler("start", start))
 
-    # Використання вебхуків із правильною URL-адресою без вказання порту
-    public_url = os.getenv('RENDER_EXTERNAL_URL')
-    updater.start_webhook(listen="0.0.0.0", url_path=TOKEN)
-    updater.bot.set_webhook(f"{public_url}/{TOKEN}")
-
+    # Запуск бота за допомогою long polling
+    updater.start_polling()
     updater.idle()
